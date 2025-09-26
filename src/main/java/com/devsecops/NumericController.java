b29258afb30849f49d66c66d745fd00e82fe3ce9
ABDELRAHMAN12415 <abdelrahman.aa.aa68@gmail.com>
2025-09-26 10:28:06 +0300

    fixing naming issue in NumericController.java

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -11,38 +11,35 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class NumericController {
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private static final Logger logger = LoggerFactory.getLogger(NumericController.class);
     private static final String BASE_URL = "http://node-service:5000/plusone";
     // private static final String BASE_URL = "http://localhost:5000/plusone";
 
     private final RestTemplate restTemplate = new RestTemplate();
 
-    @RestController
-    public class Compare {
-        @GetMapping("/")
-        public String welcome() {
-            return "Welcome!";
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome!";
+    }
+
+    @GetMapping("/compare/{value}")
+    public String compareToFifty(@PathVariable int value) {
+        if (value > 50) {
+            return "Greater than 50";
+        } else {
+            return "Smaller than or equal to 50";
         }
     }
 
+    @GetMapping("/increment/{value}")
+    public int increment(@PathVariable int value) {
+        ResponseEntity<String> responseEntity =
+                restTemplate.getForEntity(BASE_URL + '/' + value, String.class);
 
-		@GetMapping("/compare/{value}")
-		public String compareToFifty(@PathVariable int value) {
-			String message = "Could not determine comparison";
-			if (value > 50) {
-				message = "Greater than 50";
-			} else {
-				message = "Smaller than or equal to 50";
-			}
-			return message;
-		}
+        String response = responseEntity.getBody();
+        logger.info("Value Received in Request - {}", value);
+        logger.info("Node Service Response - {}", response);
 
-		@GetMapping("/increment/{value}")
-		public int increment(@PathVariable int value) {
-			ResponseEntity<String> responseEntity = restTemplate.getForEntity(baseURL + '/' + value, String.class);
-			String response = responseEntity.getBody();
-			logger.info("Value Received in Request - " + value);
-			logger.info("Node Service Response - " + response);
-			return Integer.parseInt(response);
-		}
-	}
+        return Integer.parseInt(response);
+    }
+}
