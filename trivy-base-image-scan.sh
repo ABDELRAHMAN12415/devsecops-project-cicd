9a463d44ec147d8f9ee6529c74fb7a8c4afb93d6
ABDELRAHMAN12415 <abdelrahman.aa.aa68@gmail.com>
2025-09-28 11:32:48 +0300

    using trivy to scan base images in DockerFile

--- trivy-base-image-scan.sh
+++ trivy-base-image-scan.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-set -e  # exit immediately if a command fails
 
 # Scan all base images in the Dockerfile for vulnerabilities using Trivy
 awk '$1 == "FROM" ? system("docker run --rm -v $HOME/trivy-cache:/root/.cache/ aquasec/trivy:latest image --severity CRITICAL --exit-code 1 --quiet " $2) : 0' Dockerfile
 
 # The script will exit with a non-zero status if any base image has CRITICAL vulnerabilities
 exit_code=$?
-if [[ "${exit_code}" == 1 ]]; then
-    echo "Base image scanning failed. Vulnerabilities found"
+if [[ "${exit_code}" != 0 ]]; then
+    echo "Base image scanning failed or Vulnerabilities found"
     exit 1;
 else
     echo "Base image scanning passed. No CRITICAL vulnerabilities found"
