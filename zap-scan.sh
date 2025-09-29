650569017b2360351b2ce121711199dd3cf4f764
ABDELRAHMAN12415 <abdelrahman.aa.aa68@gmail.com>
2025-09-29 12:34:01 +0300

    ZAP-scan file depug

--- zap-scan.sh
+++ zap-scan.sh
@@ -3,8 +3,13 @@
 PORT=$(kubectl -n default get svc devsecops-svc -o json | jq .spec.ports[].nodePort)
 
 echo "SERVICE_PORT=$PORT"
+echo "APPLICATION_URL=$applicationURL"
+echo " $(pwd) "
+# first run this
+#chmod 777 $(pwd)
+#echo $(id -u):$(id -g)
 
-docker run  --rm -v $(pwd):/zap/wrk/:rw   -t zaproxy/zap-stable   zap-api-scan.py     -t $applicationURL:$PORT/v3/api-docs     -f openapi     -r zap-report.html
+docker run --rm -v $(pwd):/zap/wrk/:rw -t zaproxy/zap-stable zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap-report.html
 
 exit_code=$?
 
